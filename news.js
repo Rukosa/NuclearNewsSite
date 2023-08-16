02179dfa2bc82f0c81603d2ccef67067ac771e8a
Rukosa <mwmcginnis490@gmail.com>
2023-08-16 12:15:46 -0500

    Promises/Cleanup fix
    Changed functions to all async versions and used promises to keep things running in order. This fixes the use of a couple async functions causing the categories to run out of order.
    General cleanup and organization done as well.

--- news.js
+++ news.js
@@ -3,42 +3,61 @@
 //I cannot make a request to the API without hosting a webserver so this is just proof of concept
 //TODO:
 /* 
-Write to filtered_html.txt tagged html categories (maybe by div?) and update variables in news_page.js
+Update variables in news_page.js
 Fix generation bugs in the html
 Auto update the html
 Move this to a server and automate
 */
 
 //Constants
-const fs = require('fs');
+const fs = require('fs').promises;
 const categories = ["news", "nuclear", "political", "quantum"];
-const json_txt_file = '/home/tina/Documents/Workspace/NuclearNewsSite/';
+const json_txt_file = '/home/tina/Documents/Workspace/NuclearNewsSite/json_txt_files/';
 const filtered_html_txt_file = '/home/tina/Documents/Workspace/NuclearNewsSite/filtered_html.txt';
 
-function writeFilteredHtml(text_html, filepath){
+//Appends html to a file
+async function writeFilteredHtml(text_html, filepath) {
+    try {
+        //Think about changing this to making a write stream for scaling..
+        /*
     var stream = fs.createWriteStream(filepath, {flags:'a'});
     stream.write(text_html);
-    stream.end();
+    stream.end();*/
+        await fs.appendFile(filepath, text_html)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+//Removes uneeded files for replacement
+async function removeFile(filepath) {
+    try {
+        await fs.unlink(filepath);
+        console.log("Removed " + filepath);
+    } catch (err) {
+        console.log(err)
+    }
 }
 
 //Generates html given a category. Uses the JSON txt files
-function generate_html(category, filepath){
-
-    fs.readFile(filepath + category + '.txt', (err, inputD) => {
-        if (err) throw err;
+async function generate_html(category, filepath) {
+    try {
+        //Reads files then parses JSON into string
+        const inputD = await fs.readFile(filepath + category + '.txt');
         const json_obj = JSON.parse(inputD.toString());
 
-        //Articles to html
-        for(let i = 0; i < 20; i++){
+        //Convert articles to html
+        for (let i = 0; i < 20; i++) {
             //Removes MOST unneccessary parts of the string
             var formattedContent = JSON.stringify(json_obj.articles[i].content).replace(/ \[(.*?)\] */g, "");
             formattedContent = formattedContent.replace(/\\n/g, '');
             formattedContent = formattedContent.replace(/\\r/g, '');
 
-            //First article
-            if(i == 0 || i == 1 || i == 2){
-                var first_article = 
-                `
+
+            if (i < 3) {
+                //First articles styling
+                var first_article =
+                    `
                 <!--${category}-->
                 <div class="${category}">
                 <h1 class="news_header">${json_obj.articles[i].source.name}</h1>
@@ -48,12 +67,12 @@ function generate_html(category, filepath){
                 <p class="news_p_first">${formattedContent}</p>
                 </div>
                 `
-                writeFilteredHtml(first_article, filtered_html_txt_file);
+                await writeFilteredHtml(first_article, filtered_html_txt_file);
                 //console.log(first_article);
-            } else{
-                //The rest of the articles
+            } else {
+                //The rest of the articles styling
                 var article =
-                `
+                    `
                 <div class="in ${category}">
                 <h2>${json_obj.articles[i].source.name}</h2>
                 <img class="news_image" src="${json_obj.articles[i].urlToImage}">
@@ -62,88 +81,32 @@ function generate_html(category, filepath){
                 <p>${formattedContent}</p>
                 </div>
                 `
-                writeFilteredHtml(article, filtered_html_txt_file);
+                await writeFilteredHtml(article, filtered_html_txt_file);
                 //console.log(article);
             }
         }
         console.log(category + " written successfully");
-    })
-}
-
-try{
-    fs.unlinkSync(filtered_html_txt_file);
-    console.log("Removed " + filtered_html_txt_file);
-} catch(err){
-    console.log(err);
-}
-
-console.log("Attempting write..")
-categories.forEach((category)=> generate_html(category, json_txt_file));
-
-//Code to remove
-function fillArticles(){
-    for(let i = 0; i < 20; i++){
-        //Removes MOST unneccessary parts of the string
-        var formattedContent = JSON.stringify(json_obj.articles[i].content).replace(/ \[(.*?)\] */g, "");
-        formattedContent = formattedContent.replace(/\\n/g, '');
-        formattedContent = formattedContent.replace(/\\r/g, '');
-
-        //First articles for display in carousel
-        if(i == 0 || i == 1 || i == 2){
-            var first_article = 
-            `
-            <div>
-            <h1>${json_obj.articles[i].source.name}</h1>
-            <img class="news_image" src="${json_obj.articles[i].urlToImage}">
-            <br>
-            <a class="news_desc" href="${json_obj.articles[i].url}">${json_obj.articles[i].title}</a>
-            <p>${formattedContent}</p>
-            </div>
-            `
-            console.log(first_article);
-        } else{
-            //The rest of the articles
-            var article =
-            `
-            <div>
-            <h2>${json_obj.articles[i].source.name}</h2>
-            <img class="news_image" src="${json_obj.articles[i].urlToImage}">
-            <br>
-            <a class="news_desc" href="${json_obj.articles[i].url}">${json_obj.articles[i].title}</a>
-            <p>${formattedContent}</p>
-            </div>
-            `
-            console.log(article);
-        }
+    } catch (err) {
+        console.log(err);
     }
 }
 
-/*Here is html examples for filling in ... subject to change
-<div>
-    <h1 class="news_header_first">Mirror Online</h1>
-    <img class="news_image_first" width="500px" height="auto" src="https://i2-prod.mirror.co.uk/incoming/article29741979.ece/ALTERNATES/s1200/0_Tu95-5_east2west-news.jpg">
-    <br>
-    <a class="news_desc_first" href="https://www.mirror.co.uk/news/world-news/vladimir-putin-orders-two-bear-29741435">Vladimir Putin orders two 'Bear' nuclear bomber jets to border in chilling threat</a>
-    <p class="news_p_first">"Vladimir Putin has ordered two Tu-95MS nuclear bombers to the border as part of surprise drills believed to be a new show of strength to the West. Footage shows the noisy \"Bears\" flying over Russia…"</p>
-</div>
 
-<div>
-    <h1 class="news_header_first">Mirror Online</h1>
-    <a class="news_desc_first" href="https://www.mirror.co.uk/news/world-news/vladimir-putin-orders-two-bear-29741435">Vladimir Putin orders two 'Bear' nuclear bomber jets to border in chilling threat</a>
-    <p class="news_p_first">"Vladimir Putin has ordered two Tu-95MS nuclear bombers to the border as part of surprise drills believed to be a new show of strength to the West. Footage shows the noisy \"Bears\" flying over Russia…"</p>
-</div>
 
-<div>
-    <h2>Mirror Online</h2>
-    <img class="news_image" src="https://i2-prod.mirror.co.uk/incoming/article29741979.ece/ALTERNATES/s1200/0_Tu95-5_east2west-news.jpg">
-    <br>
-    <a class="news_desc" href="https://www.mirror.co.uk/news/world-news/vladimir-putin-orders-two-bear-29741435">Vladimir Putin orders two 'Bear' nuclear bomber jets to border in chilling threat</a>
-    <p>"Vladimir Putin has ordered two Tu-95MS nuclear bombers to the border as part of surprise drills believed to be a new show of strength to the West. Footage shows the noisy \"Bears\" flying over Russia…"</p>
-</div>
+//Driver code
+async function processCategories() {
+    try {
+        await removeFile(filtered_html_txt_file);
 
-<div class="news">
-    <h2>Mirror Online</h2>
-    <a class="news_desc" href="https://www.mirror.co.uk/news/world-news/vladimir-putin-orders-two-bear-29741435">Vladimir Putin orders two 'Bear' nuclear bomber jets to border in chilling threat</a>
-    <p>"Vladimir Putin has ordered two Tu-95MS nuclear bombers to the border as part of surprise drills believed to be a new show of strength to the West. Footage shows the noisy \"Bears\" flying over Russia…"</p>
-</div>
-*/
+        console.log("Attempting write..");
+
+        for (const category of categories) {
+            await generate_html(category, json_txt_file);
+        }
+        //categories.forEach((category)=> generate_html(category, json_txt_file));
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+processCategories();
